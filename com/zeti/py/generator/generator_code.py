f583805af650682e8ce8f2f4be54e2ab84b3213f
zeti <zmk95511@163.com>
2020-12-29 11:47:38 +0800

    连接数据库，并展示所有库名并设置checkbox勾选提交

--- com/zeti/py/generator/generator_code.py
+++ com/zeti/py/generator/generator_code.py
@@ -1,10 +1,14 @@
+import json
 import flask
 
 from sqlalchemy import create_engine, MetaData, inspect
 from sqlalchemy.ext.declarative import declarative_base
 from flask import request
+from flask_cors import CORS
 
 app = flask.Flask(__name__)
+CORS(app, supports_credentials=True)
+CORS(app, resources=r'/*')
 
 
 # mysql连接引擎，echo开启调试
@@ -30,6 +34,7 @@ def get_database_all_tables(engine):
         print('table: ', name)
     return names
 
+
 # inspector = inspect(engine)
 # schemas = inspector.get_schema_names()  # 该链接下所有的库
 # for schema in schemas:
@@ -44,6 +49,7 @@ def get_database_all_tables(engine):
 
 @app.route("/connect", methods=['POST'])
 def connect():
+    """连接数据库，读取数据库表，返回列表渲染页面"""
     database_host = request.form.get('database_host')
     database_port = request.form.get('database_port')
     database_base_name = request.form.get('database_baseName')
@@ -51,23 +57,24 @@ def connect():
     database_password = request.form.get('database_password')
 
     if database_host is None or len(database_host) == 0:
-        return '数据库IP不能为空'
+        return json.dumps({'msg': '数据库IP不能为空', 'code': '-1', 'data': ''})
 
     if database_port is None or len(database_port) == 0:
         database_port = 3306
 
     if database_base_name is None or len(database_base_name) == 0:
-        return '数据库名称不能为空'
+        return json.dumps({'msg': '数据库名称不能为空', 'code': '-1', 'data': ''})
 
     if database_user_name is None or len(database_user_name) == 0:
-        return '用户名不能为空'
+        return json.dumps({'msg': '用户名不能为空', 'code': '-1', 'data': ''})
 
     if database_password is None or len(database_password) == 0:
-        return '密码不能为空'
+        return json.dumps({'msg': '密码不能为空', 'code': '-1', 'data': ''})
 
     database = connect_database(database_host, database_port, database_base_name, database_user_name, database_password)
-    return str(get_database_all_tables(database))
+    tables = get_database_all_tables(database)
+    return json.dumps({'msg': 'success', 'code': '1', 'data': tables})
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=8001, host='localhost')
