41c8a6fb2181cb3a95967afe3d2f98314c06492f
zeti <zmk95511@163.com>
2021-02-21 08:40:08 +0800

    commit

--- com/zeti/py/generator/generator_code.py
+++ com/zeti/py/generator/generator_code.py
@@ -3,8 +3,9 @@ import flask
 
 from sqlalchemy import create_engine, MetaData, inspect
 from sqlalchemy.ext.declarative import declarative_base
-from flask import request, Blueprint
+from flask import request, Blueprint, render_template
 from flask_cors import CORS
+import os
 
 app = flask.Flask(__name__)
 CORS(app, supports_credentials=True)
@@ -46,6 +47,10 @@ def get_database_all_tables(engine):
 #         for column in inspector.get_columns(table_name, schema=schema):
 #             print("Column: %s" % column)
 
+@app.route('/index')
+def index():
+    return render_template('create_class.html')
+
 
 @app.route("/connect", methods=['POST'])
 def connect():
@@ -56,22 +61,23 @@ def connect():
     database_user_name = request.form.get('database_userName')
     database_password = request.form.get('database_password')
 
-    if database_host is None or len(database_host) == 0:
-        return json.dumps({'msg': '数据库IP不能为空', 'code': '-1', 'data': ''})
+    # if database_host is None or len(database_host) == 0:
+    #     return json.dumps({'msg': '数据库IP不能为空', 'code': '-1', 'data': ''})
+    #
+    # if database_port is None or len(database_port) == 0:
+    #     database_port = 3306
+    #
+    # if database_base_name is None or len(database_base_name) == 0:
+    #     return json.dumps({'msg': '数据库名称不能为空', 'code': '-1', 'data': ''})
+    #
+    # if database_user_name is None or len(database_user_name) == 0:
+    #     return json.dumps({'msg': '用户名不能为空', 'code': '-1', 'data': ''})
+    #
+    # if database_password is None or len(database_password) == 0:
+    #     return json.dumps({'msg': '密码不能为空', 'code': '-1', 'data': ''})
 
-    if database_port is None or len(database_port) == 0:
-        database_port = 3306
-
-    if database_base_name is None or len(database_base_name) == 0:
-        return json.dumps({'msg': '数据库名称不能为空', 'code': '-1', 'data': ''})
-
-    if database_user_name is None or len(database_user_name) == 0:
-        return json.dumps({'msg': '用户名不能为空', 'code': '-1', 'data': ''})
-
-    if database_password is None or len(database_password) == 0:
-        return json.dumps({'msg': '密码不能为空', 'code': '-1', 'data': ''})
-
-    database = connect_database(database_host, database_port, database_base_name, database_user_name, database_password)
+    # database = connect_database(database_host, database_port, database_base_name, database_user_name, database_password)
+    database = connect_database('106.13.22.217', '3306', 'delta_medical', 'root', 'mysql123')
     tables = get_database_all_tables(database)
     return json.dumps({'msg': 'success', 'code': '1', 'data': tables})
 
@@ -88,20 +94,44 @@ def generator():
     dao = request.form.get('dao')
     entity = request.form.get('entity')
 
+    if controller == 0 & service == 0 & dao == 0 & entity == 0:
+        return json.dumps({'msg': '请至少选择一个需要生成的module', 'code': '-1', 'data': ''})
+
     """method"""
     page = request.form.get('page')
     list = request.form.get('list')
+    selectById = request.form.get('selectById')
     save = request.form.get('save')
     update = request.form.get('update')
     deleteById = request.form.get('deleteById')
-    selectById = request.form.get('selectById')
 
-    str = '{"package_url": ' + package_url + ', "author": ' + author + '}'
+    """select databases"""
+    databases = request.form.get('selectDatabases')
+
+    """generator template"""
+    if databases is None:
+        return json.dumps({'msg': '请至少选择一个需要生成的表', 'code': '-1', 'data': ''})
+
+    split = databases.split(",")
+    for i in split:
+        print(i)
+
+    str = '{"package_url": ' + package_url + ', "author": ' + author + ', "databases": ' + databases + '}'
 
     print(str)
 
     return json.dumps({'msg': 'success', 'code': '1', 'data': str})
 
 
+# 创建java文件
+def create_file(class_name, package, content, suffix='.java'):
+    dirs = os.getcwd() + '/file/' + package.replace('.', '/') + '/'
+    if not os.path.exists(dirs):
+        os.makedirs(dirs, 0o777)
+    fd = os.open(dirs + class_name + suffix, os.O_WRONLY | os.O_CREAT)
+    os.write(fd, content.encode(encoding="utf-8", errors="strict"))
+    os.close(fd)
+
 if __name__ == '__main__':
-    app.run(debug=True, port=8001, host='localhost')
+    # app.run(debug=True, port=8001, host='localhost')
+    create_file('test', 'package', 'text')
